b3a83f02bfd55c481ddac73828762e74260220ff
JulianaSouza49 <lj.m.msouza@gmail.com>
2025-12-02 16:59:03 -0300

    Add files via upload

---
+++ divisaodedoisnumeros.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+float numero01, numero02, divisao;
+printf("digite o 1 numero: ");
+scanf("%f",&numero01);
+printf("digite o 2 numero: ");
+scanf("%f", &numero02);
+divisao = numero01 / numero02;
+    printf("a divisao eh: %.2f", divisao);
+return 0;
+
+}
